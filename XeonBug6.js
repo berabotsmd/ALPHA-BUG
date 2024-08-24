0d7313965ef11b241ebfb3e182e2fb501b28a2db
berabotsmd <berahiggins@gmail.com>
2024-08-24 17:03:58 +0300

    Update XeonBug6.js

--- XeonBug6.js
+++ XeonBug6.js
@@ -54,9 +54,9 @@ const ZipXeon = JSON.parse(fs.readFileSync('./database/autoreply/zip.json'))
 const ApkXeon = JSON.parse(fs.readFileSync('./database/autoreply/apk.json'))
 
 //time
-const xtime = moment.tz('Africa/Lagos').format('HH:mm:ss')
-        const xdate = moment.tz('Africa/Lagos').format('DD/MM/YYYY')
-        const time2 = moment().tz('Africa/Lagos').format('HH:mm:ss')  
+const xtime = moment.tz('Africa/Nairobi').format('HH:mm:ss')
+        const xdate = moment.tz('Africa/Nairobi').format('DD/MM/YYYY')
+        const time2 = moment().tz('Africa/Nairobi').format('HH:mm:ss')  
          if(time2 < "23:59:00"){
 var xeonytimewisher = `Good Night 🌌`
  }
